704e81010d4448763350068a147498faad79396a
JieCheng <ninthmelody@163.com>
2022-10-03 22:53:23 +0800

    feat: test

--- index.ts
+++ index.ts
@@ -1,18 +1,4 @@
-import { readdirSync, stat, lstatSync } from 'node:fs';
-import path = require('node:path');
 import { DirectoryWatcher, EventType } from './diractory-watcher';
-
-export function test() {
-  const dir = './';
-  const files = readdirSync(dir);
-  files.forEach((file) => {
-    const filePath = path.join(dir, file);
-    const fileStat = lstatSync('foo');
-    console.log(filePath);
-    console.log(fileStat);
-    console.log(fileStat.isDirectory());
-  });
-}
 
 const watcher = new DirectoryWatcher('./');
 
